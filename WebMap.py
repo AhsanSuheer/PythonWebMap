0d4d6a8b51396bb26ee4c87a346938a021088e87
ahsansuheer <ahsan.suheer@gmail.com>
2019-11-27 23:31:25 -0500

    Added a pointer to my home.

--- WebMap.py
+++ WebMap.py
@@ -1,6 +1,10 @@
 import folium
 
-myMap = folium.Map(location = [33.651834, 73.050557], zoom_start = 20)
+myMap = folium.Map(location = [34.651834, 73.050557], zoom_start = 20)
+
+myFG = folium.FeatureGroup(name = "Points")
+myFG.add_child(folium.Marker(location = [34.651810, 73.050738], popup = "This is my house.", icon = folium.Icon(color = 'blue')))
+myMap.add_child(myFG)
 
 myMap.save("home.html")
 
